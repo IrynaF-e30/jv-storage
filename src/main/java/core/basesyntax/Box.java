772ad1d7463cff3dce5b69e5f7faa7a586059cb1
Iryna Finenko <irynafinenko@gmail.com>
2025-07-25 21:13:37 +0300

    fixed checkstyle problem

---
+++ src/main/java/core/basesyntax/Box.java
@@ -0,0 +1,48 @@
+package core.basesyntax;
+
+public class Box {
+    private final String name;
+    private final String color;
+
+    public Box() {
+        this.name = "default";
+        this.color = "gray";
+    }
+
+    public Box(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return "Box{name='" + name + "', color='" + color + "'}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Box box = (Box) o;
+        return name.equals(box.name) && color.equals(box.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode() + 31 * color.hashCode();
+    }
+}
+
+
--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -3,13 +3,19 @@ package core.basesyntax;
 public class Main {
     public static void main(String[] args) {
         Storage<Integer, Box> storage = new StorageImpl<>();
-        Box box = new Box();
-        storage.put(22, box);
 
-        Box value = storage.get(22);
-        System.out.println(value); // має вивести Box@...
+        Box box1 = new Box("Myrchyk", "white");
+        Box box2 = new Box("Barsyk", "black");
+        Box box3 = new Box(); // використання конструктора без аргументів
 
-        System.out.println(storage.size()); // має вивести 1
+        storage.put(1, box1);
+        storage.put(2, box2);
+        storage.put(3, box3);
+
+        System.out.println(storage.get(1)); // Box{name='Myrchyk', color='white'}
+        System.out.println(storage.get(2)); // Box{name='Barsyk', color='black'}
+        System.out.println(storage.get(3)); // Box{name='default', color='gray'}
+        System.out.println("Size: " + storage.size()); // 3
     }
 }
 
